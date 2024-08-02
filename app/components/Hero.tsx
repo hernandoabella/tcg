e735903f53073ed0fd459e59559d41a120472f0d
Hernando Abella <hernandoabella@gmail.com>
2024-08-01 22:35:12 -0500

    Create Hero.tsx

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Testimonial Card Generator</h1>
+      <p>Create stunning cards testimonials for your awesome products</p>
+    </div>
+  );
+};
+
+export default Hero;
